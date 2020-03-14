6ba3a7631ba449e93c48e6400ae64394b375c84f
OrangeJedi <orange.jedi@outlook.com>
2020-03-14 13:52:22 -0600

    Choose videos by type

--- web/config.js
+++ web/config.js
@@ -4,7 +4,7 @@ const store = new Store();
 
 let allowedVideos = store.get("allowedVideos");
 
-if(store.get('clock')){
+if (store.get('clock')) {
     console.log('hello');
     document.getElementById("showClock").checked = true;
 }
@@ -27,35 +27,106 @@ function changeTab(evt, tab) {
     evt.currentTarget.className += " w3-blue";
 }
 
-$(document).ready(()=>{
-     let videoList = "<h3 class=\"w3-bar-item\"><i class=\"fa fa-film\"></i> Videos</h3>";
-     console.log(videos.length);
-    for(let i = 0; i < videos.length;i++){
-        videoList += `<span><input type="checkbox" ${allowedVideos.includes(videos[i].id) ? "checked" : ""} class="w3-check" onclick="checkVideo(event,${i})">
+$(document).ready(() => {
+    makeList();
+    selectVideo(-1);
+});
+
+function makeList() {
+    let videoList = "<a onclick=\"selectVideo(-1)\"><h3 class=\"w3-bar-item w3-deep-orange videoListItem\" id='videoListTitle'><i class=\"fa fa-film\"></i> Videos</h3></a>";
+    let headertxt = "";
+    for (let i = 0; i < videos.length; i++) {
+        if (headertxt !== videos[i].accessibilityLabel) {
+            videoList += `<h5 class="w3-bar-item videoListItem" id='videoListTitle'>${videos[i].accessibilityLabel}</h5>`;
+            headertxt = videos[i].accessibilityLabel;
+        }
+        videoList += `<span style="padding-left: 10%; font-size: small"><input type="checkbox" ${allowedVideos.includes(videos[i].id) ? "checked" : ""} class="w3-check" onclick="checkVideo(event,${i})">
                       <a style="display: inline;" href="#" id="videoList-${i}" onclick="selectVideo(${i})" class="w3-bar-item w3-button videoListItem">
                       ${videos[i].name ? videos[i].name : videos[i].accessibilityLabel}
-                      </a><br>`;
+                      </a></span><br>`;
     }
-    videoList += "<br><br><br><br><br>";
+    videoList += "<br><br><br><br><br><br>";
     $('#videoList').html(videoList);
-});
+}
 
 function selectVideo(index) {
     let x = document.getElementsByClassName("videoListItem");
     for (i = 0; i < x.length; i++) {
         x[i].className = x[i].className.replace("w3-deep-orange", "");
     }
-    document.getElementById("videoList-" + index).className += " w3-deep-orange";
-    $('#videoPlayer').attr("src", videos[index].src.H2641080p);
-    $('#videoName').text(videos[index].accessibilityLabel);
+    if (index > -1) {
+        document.getElementById("videoList-" + index).className += " w3-deep-orange";
+        $('#videoPlayer').attr("src", videos[index].src.H2641080p).show();
+        $('#videoName').text(videos[index].accessibilityLabel);
+        $('#videoSettings').html("");
+    } else {
+        document.getElementById("videoListTitle").className += " w3-deep-orange";
+        $('#videoPlayer').attr("src", "").hide();
+        $('#videoName').text("Video Settings");
+        $('#videoSettings').html(`<br>
+                                  <div class="w3-container">
+                                  <button class="w3-button w3-white w3-border w3-border-green w3-round-large" onclick="selectAll()">Select All</button>
+                                  <button class="w3-button w3-white w3-border w3-border-red w3-round-large" onclick="deselectAll()">Deselect All</button>
+                                  <br><br>
+                                  <select class="w3-select w3-border" style="width: 25%" id="videoType">
+                                     <option value="">Aerial</option>
+                                     <option value="space">Space</option>
+                                     <option value="underwater">Underwater</option>
+                                  </select> 
+                                  <button class="w3-button w3-white w3-border w3-border-green w3-round-large" onclick="selectType()">Select Type</button>
+                                  <button class="w3-button w3-white w3-border w3-border-red w3-round-large" onclick="deselectType()">Deselect Type</button>
+                                  </div>`);
+    }
 }
 
-function checkVideo(e,index) {
-    if(e.currentTarget.checked){
+function checkVideo(e, index) {
+    if (e.currentTarget.checked) {
         allowedVideos.push(videos[index].id);
-    }else{
-        allowedVideos.splice(allowedVideos.indexOf(videos[index].id),1);
+    } else {
+        allowedVideos.splice(allowedVideos.indexOf(videos[index].id), 1);
     }
     store.set("allowedVideos", allowedVideos);
-    console.log(allowedVideos.length, store.get('allowedVideos').length);
+}
+
+function deselectAll() {
+    allowedVideos = [];
+    store.set("allowedVideos", allowedVideos);
+    makeList();
+}
+
+function selectAll() {
+    allowedVideos = [];
+    for (let i = 0; i < videos.length; i++) {
+        allowedVideos.push(videos[i].id);
+    }
+    store.set("allowedVideos", allowedVideos);
+    makeList();
+}
+
+function selectType() {
+    let type = $('#videoType').val();
+    type = type === "" ? undefined : type;
+    for (let i = 0; i < videos.length; i++) {
+        if (videos[i].type === type) {
+            if (!allowedVideos.includes(videos[i].id)) {
+                allowedVideos.push(videos[i].id);
+            }
+        }
+    }
+    store.set("allowedVideos", allowedVideos);
+    makeList();
+}
+
+function deselectType() {
+    let type = $('#videoType').val();
+    type = type === "" ? undefined : type;
+    for (let i = 0; i < videos.length; i++) {
+        if (videos[i].type === type) {
+            if (allowedVideos.includes(videos[i].id)) {
+                allowedVideos.splice(allowedVideos.indexOf(videos[i].id), 1);
+            }
+        }
+    }
+    store.set("allowedVideos", allowedVideos);
+    makeList();
 }
