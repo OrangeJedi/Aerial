9608665f8ec50aa4c024045dd1992ac15f0a5f37
OrangeJedi <orange.jedi@outlook.com>
2022-10-05 17:33:23 -0600

    Updated version

--- web/screensaver.js
+++ web/screensaver.js
@@ -232,35 +232,3 @@ electron.ipcRenderer.on('blankTheScreen', () => {
         video.src = "";
     },transitionLength);
 });
-
-(function(w) {
-    var oldST = w.setTimeout;
-    var oldSI = w.setInterval;
-    var oldCI = w.clearInterval;
-    var timers = [];
-    w.timers = timers;
-    w.setTimeout = function(fn, delay) {
-        var id = oldST(function() {
-            fn && fn();
-            removeTimer(id);
-        }, delay);
-        timers.push(id);
-        return id;
-    };
-    w.setInterval = function(fn, delay) {
-        var id = oldSI(fn, delay);
-        timers.push(id);
-        return id;
-    };
-    w.clearInterval = function(id) {
-        oldCI(id);
-        removeTimer(id);
-    };
-    w.clearTimeout = w.clearInterval;
-
-    function removeTimer(id) {
-        var index = timers.indexOf(id);
-        if (index >= 0)
-            timers.splice(index, 1);
-    }
-}(window));
